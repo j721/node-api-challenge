47fed774ce205f0af557348a151699200e67310f
j721 <juliadvu@gmail.com>
2020-05-08 11:24:54 -0500

    crud added to projectRouter

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -12,6 +12,26 @@ router.get('/', (req, res)=>{
     })
 })
 
+router.get('/:id', (req, res)=>{
+    projects.get(req.params.id)
+    .then((project)=>{
+        res.status(200).json(project)
+    })
+    .catch((error)=>{
+        res.status(500).json({error: "unable to get project by id"})
+    })
+})
+
+router.get('/:id/actions', (req,res)=>{
+    projects.getProjectActions(req.params.id)
+    .then((action)=>{
+        res.status(200).json(action)
+    })
+    .catch((error)=>{
+        res.status(500).json({error: "unable to get project actions"})
+    })
+})
+
 router.post('/', (req,res)=>{
     const body = req.body;
     if(body.description && body.name){
@@ -27,6 +47,26 @@ router.post('/', (req,res)=>{
     }
 })
 
+router.put('/:id', (req,res)=>{
+    projects.update(req.params.id, req.body)
+    .then((project)=>{
+        res.status(200).json(project)
+    })
+    .catch((error)=>{
+        res.status(500).json({error: "unable to update project."})
+    })
+})
+
+
+router.delete('/:id', (req,res)=>{
+    projects.remove(req.params.id)
+    .then((project)=>{
+        res.status(200).json(project)
+    })
+    .catch((error)=>{
+        res.status(500).json({error: "failed to remove project."})
+    })
+})
 
 //custom middleware
 
