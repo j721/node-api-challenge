ccc83a6586fdcbcc11c4c6a2304d1e1830ad3c16
j721 <juliadvu@gmail.com>
2020-05-08 10:43:23 -0500

    updates to index.js and server.js

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 const express = require('express');
+const server = express();
+server.use(express.json());
 
 const projectRouter = require('./routes/projectRouter');
 const actionRouter = require('./routes/actionRouter');
 
-/*const cors = require('cors')
-  server.use(cors());
-*/
-const server = express();
+server.use('/api/project', projectRouter);
+server.use('/api/action', actionRouter);
+
 server.use(express.json());
-
-server.use("/api/projects", projectRouter);
-server.use("/api/actions", actionRouter);
-
-server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Actions and Projects!</h2>`);
-  res.status(200).json({environment: process.env.NODE_ENV});
 });
 
-//custom middleware
-function logger(req, res, next) {
-  console.log(`${req.method} from ${req.originalUrl} at ${new Date().toISOString()}`)
-  next();
-}
-
 module.exports = server;
+
+//   res.status(200).json({environment: process.env.NODE_ENV});
+//custom middleware
+// function logger(req, res, next) {
+//   console.log(`${req.method} from ${req.originalUrl} at ${new Date().toISOString()}`)
+//   next();
+// }
+
