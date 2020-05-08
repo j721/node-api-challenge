0eaaca7ed5a8d0a3b908ca8040a6c49d8fb32cce
j721 <juliadvu@gmail.com>
2020-05-08 11:11:19 -0500

    post request for projectRouter

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const project = require('../data/helpers/projectModel');
+const projects = require('../data/helpers/projectModel');
 
 router.get('/', (req, res)=>{
-    project.get()
+    projects.get()
     .then(project=>{
         res.status(200).json(project)
     })
@@ -11,3 +11,40 @@ router.get('/', (req, res)=>{
         res.status(500).json({error: "unable to get projects."})
     })
 })
+
+router.post('/', (req,res)=>{
+    const body = req.body;
+    if(body.description && body.name){
+        projects.insert(body)
+        .then((project)=>{
+            res.status(200).json(project)
+        })
+        .catch((error)=>{
+            res.status(500).json({error: "Sorry, could not add project."})
+        })
+    }else{
+        return res.status(400).json({message: "description and name required."})
+    }
+})
+
+
+//custom middleware
+
+// function validateProjectId(){
+//     return (req,res,next)=>{
+//        if(req.params.id){
+//         projects.get(req.params.id)
+//         .then((project)=>{
+//             if(project){
+//                 req.project = project;
+//                 next();
+//             }else{
+//                 res.status(400).json({message: "sorry, project not found."})
+//             }
+//         })
+//         .catch((err)=>{
+//             next(err);
+//         })
+//        }
+//     }
+// }
