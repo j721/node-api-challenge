a792591ce16c9e82d47b01b543719f0150fa9e09
j721 <juliadvu@gmail.com>
2020-05-08 19:08:50 -0500

    got api to show

--- node-sprint-app/src/App.js
+++ node-sprint-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(()=>{
-    axios.get("http://localhost:6000/api/project")
+    axios.get('http://localhost:6000/api/project/')
     .then((res)=>{
       console.log(res, 'data returned')
       setData(res.data.data)
@@ -23,9 +23,9 @@ function App() {
       <h1>Projects</h1>
       <Route exact path ="/">
         <div className="project holder">
-        {data.map((project)=>{
+        {data.map((project)=>(
           <Projects key = {project.id} data ={project}/>
-        })}
+        ))}
         </div>
       </Route>
 
@@ -38,3 +38,45 @@ function App() {
 }
 
 export default App;
+
+// import React, { useEffect, useState } from "react";
+// import { Route } from "react-router-dom";
+// import axios from "axios";
+// import Projects from "./components/Projects";
+// import Project from "./components/Project";
+
+// function App() {
+//   const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//     axios
+//       .get("http://localhost:5000/api/projects")
+//       .then((res) => {
+//         console.log(res);
+//         setData(res.data);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }, []);
+//   return (
+//     <div className="main">
+//       <h1> Projects </h1>
+//       <Route exact path="/">
+//         <div className="project-container">
+//           {data.map((project) => (
+//             <Projects key={project.id} data={project} />
+//           ))}
+//         </div>
+//       </Route>
+
+//       <Route exact path="/project/:id">
+//         <Project data={data} />
+//       </Route>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
